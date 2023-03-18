a57afd7c51c73d8bd3b0953d969e1dc179e5343a
Ethan Cavill <ethancavill@gmail.com>
2023-03-18 14:09:41 +1030

    Install frontend deps

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,21 @@
+import "@styles/globals.css";
+import { Inter } from "@next/font/google";
+import Nav from "@components/nav";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en">
+			<head />
+			<body className={inter.className}>
+				<Nav />
+				<main>{children}</main>
+			</body>
+		</html>
+	);
+}
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,3 @@
+export default async function Home() {
+	return <div className=" mx-auto mt-6 mb-6 max-w-lg px-3">Hello World</div>;
+}
