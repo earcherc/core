eefacebcf84cd8b4c00c6dc8eb3218eb9efe61b0
Ethan Cavill <ethancavill@gmail.com>
2023-05-11 10:58:22 +0930

    Auth service: update docker-compose and dockerfile to fix errors, hide defaults from config.py and move all to root .env

--- backend/auth/app/config.py
+++ backend/auth/app/config.py
@@ -3,8 +3,8 @@ import os
 
 class Config:
     APP_ENV = os.getenv("APP_ENV", "development")
-    DATABASE_USERNAME = os.getenv("DATABASE_USERNAME", "user")
-    DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD", "password")
+    DATABASE_USERNAME = os.getenv("DATABASE_USERNAME")
+    DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
     DATABASE_HOST = os.getenv("DATABASE_HOST", "db")
-    DATABASE_NAME = os.getenv("DATABASE_NAME_AUTH", "auth_db")
+    DATABASE_NAME = os.getenv("DATABASE_NAME_AUTH")
     JWT_SECRET = os.getenv("JWT_SECRET", "your-secret-key")
