0554936ac488b535efbf4814cd14243d84dabbd3
Ethan Cavill <ethancavill@gmail.com>
2023-08-28 12:52:45 -0700

    Clean up user-profile

--- frontend/src/app/(main)/profile/page.tsx
+++ frontend/src/app/(main)/profile/page.tsx
@@ -1,26 +1,8 @@
-import { Fragment } from 'react';
-
 export default function Profile() {
   return (
     <>
       <div>
-        {/* Static sidebar for desktop */}
-        <div className="hidden xl:fixed xl:inset-y-0 xl:z-50 xl:flex xl:w-72 xl:flex-col">
-          {/* Sidebar component, swap this element with another sidebar if you like */}
-          <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-black/10 px-6 ring-1 ring-white/5">
-            <div className="flex h-16 shrink-0 items-center">
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                alt="Your Company"
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="xl:pl-72">
-          {/* Sticky search header */}
-
+        <div className="mx-auto max-w-screen-xl">
           <main>
             {/* Settings forms */}
             <div className="divide-y divide-white/5">
@@ -102,35 +84,14 @@ export default function Profile() {
                       </label>
                       <div className="mt-2">
                         <div className="flex rounded-md bg-white/5 ring-1 ring-inset ring-white/10 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
-                          <span className="flex select-none items-center pl-3 text-gray-400 sm:text-sm">
-                            example.com/
-                          </span>
                           <input
                             type="text"
                             name="username"
                             id="username"
                             autoComplete="username"
-                            className="flex-1 border-0 bg-transparent py-1.5 pl-1 text-white focus:ring-0 sm:text-sm sm:leading-6"
-                            placeholder="janesmith"
+                            className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
                           />
                         </div>
-                      </div>
-                    </div>
-
-                    <div className="col-span-full">
-                      <label htmlFor="timezone" className="block text-sm font-medium leading-6 text-white">
-                        Timezone
-                      </label>
-                      <div className="mt-2">
-                        <select
-                          id="timezone"
-                          name="timezone"
-                          className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 [&_*]:text-black"
-                        >
-                          <option>Pacific Standard Time</option>
-                          <option>Eastern Standard Time</option>
-                          <option>Greenwich Mean Time</option>
-                        </select>
                       </div>
                     </div>
                   </div>
@@ -208,44 +169,6 @@ export default function Profile() {
                       className="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
                     >
                       Save
-                    </button>
-                  </div>
-                </form>
-              </div>
-
-              <div className="grid max-w-7xl grid-cols-1 gap-x-8 gap-y-10 px-4 py-16 sm:px-6 md:grid-cols-3 lg:px-8">
-                <div>
-                  <h2 className="text-base font-semibold leading-7 text-white">Log out other sessions</h2>
-                  <p className="mt-1 text-sm leading-6 text-gray-400">
-                    Please enter your password to confirm you would like to log out of your other sessions across all of
-                    your devices.
-                  </p>
-                </div>
-
-                <form className="md:col-span-2">
-                  <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:max-w-xl sm:grid-cols-6">
-                    <div className="col-span-full">
-                      <label htmlFor="logout-password" className="block text-sm font-medium leading-6 text-white">
-                        Your password
-                      </label>
-                      <div className="mt-2">
-                        <input
-                          id="logout-password"
-                          name="password"
-                          type="password"
-                          autoComplete="current-password"
-                          className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-                        />
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="mt-8 flex">
-                    <button
-                      type="submit"
-                      className="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-                    >
-                      Log out other sessions
                     </button>
                   </div>
                 </form>
--- frontend/src/app/components/nav.tsx
+++ frontend/src/app/components/nav.tsx
@@ -19,7 +19,7 @@ export default function Nav() {
   const pathname = usePathname();
 
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav" className="sticky top-0 bg-gray-800">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
