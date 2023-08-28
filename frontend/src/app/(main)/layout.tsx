fd6682ec22f67a51983849b2529c4a66a66339ae
Ethan Cavill <ethancavill@gmail.com>
2023-08-28 12:28:48 -0700

    WIP user profile page

--- frontend/src/app/(main)/layout.tsx
+++ frontend/src/app/(main)/layout.tsx
@@ -1,10 +1,10 @@
-import Nav from '@app/_components/nav';
+import Nav from '@app/components/nav';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
+    <main className="h-full bg-gray-900">
       <Nav></Nav>
       {children}
-    </div>
+    </main>
   );
 }
