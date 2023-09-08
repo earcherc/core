c623762f2e510cea6afbb28b93d3eeee9dea2efb
Ethan Cavill <ethancavill@gmail.com>
2023-09-07 18:37:36 -0700

    Restore service files

--- backend/user/app/models/user.py
+++ backend/user/app/models/user.py
@@ -45,53 +45,6 @@ class UserProfile(SQLModel, table=True):
         ]
 
 
-class ProfilePhoto(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    user_profile_id: int = Field(default=None, foreign_key="userprofile.id", index=True)
-    url: str
-    caption: Optional[str] = Field(default=None)
-    is_main: bool
-    uploaded_at: datetime
-
-    # Relationships
-    user_profile: UserProfile = Relationship(back_populates="profile_photos")
-
-    # Constraints
-    class Config:
-        table_constraints = [
-            UniqueConstraint(
-                "user_profile_id", "is_main", name="unique_main_photo_per_user"
-            )
-        ]
-
-
-class Connection(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    user_profile1_id: int = Field(
-        default=None, foreign_key="userprofile.id", index=True
-    )
-    user_profile2_id: int = Field(
-        default=None, foreign_key="userprofile.id", index=True
-    )
-    status: ConnectionStatus  # e.g., Pending, Accepted, Blocked
-    created_at: datetime
-
-    # Relationships
-    # https://github.com/tiangolo/sqlmodel/issues/10
-    user_profile1: UserProfile = Relationship(
-        back_populates="sent_connections",
-        sa_relationship_kwargs={
-            "primaryjoin": "Connection.user_profile1_id == UserProfile.id"
-        },
-    )
-    user_profile2: UserProfile = Relationship(
-        back_populates="received_connections",
-        sa_relationship_kwargs={
-            "primaryjoin": "Connection.user_profile2_id == UserProfile.id"
-        },
-    )
-
-
 class UserProfileDetails(SQLModel, table=True):
     user_profile_id: int = Field(
         default=None, foreign_key="userprofile.id", primary_key=True
@@ -111,6 +64,4 @@ class UserProfileDetails(SQLModel, table=True):
 
 # Update forward references
 UserProfile.update_forward_refs()
-ProfilePhoto.update_forward_refs()
-Connection.update_forward_refs()
 UserProfileDetails.update_forward_refs()
---
+++ backend/user/app/services/user_service.py
@@ -0,0 +1,82 @@
+from typing import Optional
+from ..models import UserProfile as UserProfileTable
+from shared_schemas.core import (
+    UserProfileInDB,
+    UserProfileCreate,
+    UserProfileUpdate,
+)
+from sqlmodel import Session, select
+from fastapi import HTTPException
+
+
+async def get_user_profile_func(
+    user_id: int, session: Session
+) -> Optional[UserProfileInDB]:
+    user_profile_statement = select(UserProfileTable).where(
+        UserProfileTable.user_id == user_id
+    )
+    user_profile = session.exec(user_profile_statement).first()
+    if not user_profile:
+        raise HTTPException(status_code=404, detail="User profile not found")
+
+    return UserProfileInDB.from_orm(user_profile)
+
+
+async def create_user_profile_func(
+    user_profile_data: UserProfileCreate, session: Session
+):
+    existing_user_profile = session.exec(
+        select(UserProfileTable).where(
+            UserProfileTable.user_id == user_profile_data.user_id
+        )
+    ).first()
+
+    if existing_user_profile is not None:
+        raise HTTPException(
+            status_code=400, detail="User profile with this user_id already exists"
+        )
+
+    user_profile = UserProfileTable(**user_profile_data.dict())
+
+    session.add(user_profile)
+    session.commit()
+    session.refresh(user_profile)
+
+    return UserProfileInDB.from_orm(user_profile)
+
+
+async def update_user_profile_func(
+    user_id: int,
+    user_profile_data: UserProfileUpdate,
+    session: Session,
+):
+    user_profile_statement = select(UserProfileTable).where(
+        UserProfileTable.user_id == user_id
+    )
+    user_profile = session.exec(user_profile_statement).first()
+
+    if not user_profile:
+        raise HTTPException(status_code=404, detail="User profile not found")
+
+    # Only update fields that are set
+    for var, value in user_profile_data.dict(exclude_unset=True).items():
+        setattr(user_profile, var, value)
+
+    session.add(user_profile)
+    session.commit()
+    session.refresh(user_profile)
+
+    return UserProfileInDB.from_orm(user_profile)
+
+
+async def delete_user_profile_func(user_id: int, session: Session):
+    user_profile_statement = select(UserProfileTable).where(
+        UserProfileTable.user_id == user_id
+    )
+    user_profile = session.exec(user_profile_statement).first()
+    if not user_profile:
+        raise HTTPException(status_code=404, detail="User profile not found")
+
+    session.delete(user_profile)
+    session.commit()
+    return user_id
